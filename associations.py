532dbb35ec94139d89c51154e5cabb35ac5821c8
laz07 <lazm0804@gmail.com>
2022-07-17 18:19:26 -0400

    initial commit

---
+++ associations.py
@@ -0,0 +1,722 @@
+
+weapon_associations = {
+	# SCOUT
+	"tf_weapon_scattergun": {
+		"class": "Scout",
+		"slot": 1,
+		"display": "Scatterguns",
+		"all": [
+			"Scattergun",
+			"Force-A-Nature",
+			"The Back Scatter"
+		]
+	},
+	"tf_weapon_handgun_scout_primary": {
+		"class": "Scout",
+		"slot": 1,
+		"display": "Shortstop",
+		"all": [
+			"The Shortstop"
+		]
+	},
+	"tf_weapon_pep_brawler_blaster": {
+		"class": "Scout",
+		"slot": 1,
+		"display": "Baby Face's Blaster",
+		"all": [
+			"Baby Face's Blaster"
+		]
+	},
+	"tf_weapon_soda_popper": {
+		"class": "Scout",
+		"slot": 1,
+		"display": "Soda Popper",
+		"all": [
+			"The Soda Popper"
+		]
+	},
+	"tf_weapon_pistol_scout": {
+		"class": "Scout",
+		"slot": 2,
+		"display": "Most Pistols",
+		"all": [
+			"Pistol",
+			"Lugermorph",
+			"The C.A.P.P.E.R."
+		]
+	},
+	"tf_weapon_lunchbox_drink": {
+		"class": "Scout",
+		"slot": 2,
+		"display": "Bonk! Atomic Punch",
+		"all": [
+			"Bonk! Atomic Punch",
+			"Crit-a-Cola"
+		]
+	},
+	"tf_weapon_jar_milk": {
+		"class": "Scout",
+		"slot": 2,
+		"display": "Mad Milk",
+		"all": [
+			"Mad Milk",
+			"Mutated Milk"
+		]
+	},
+	"tf_weapon_handgun_scout_secondary": {
+		"class": "Scout",
+		"slot": 2,
+		"display": "Winger/Pocket Pistol",
+		"all": [
+			"The Winger",
+			"Pocket Pistol"
+		]
+	},
+	"tf_weapon_bat": {
+		"class": "Scout",
+		"slot": 3,
+		"display": "Most Melee",
+		"all": [
+			"Bat",
+			"The Candy Cane",
+			"The Boston Basher",
+			"Sun-on-a-Stick",
+			"The Fan O'War",
+			"The Atomizer",
+			"Three-Rune Blade",
+			"Batsaber",
+			"Frying Pan",
+			"Saxxy",
+			"The Conscientious Objector",
+			"The Freedom Staff",
+			"The Bat Outta Hell",
+			"The Memory Maker",
+			"The Ham Shank",
+			"Gold Frying Pan",
+			"The Necro Smasher",
+			"The Crossing Guard",
+			"Prinny Machete"
+		]
+	},
+	"tf_weapon_fish": {
+		"class": "Scout",
+		"slot": 3,
+		"display": "Holy Mackerel",
+		"all": [
+			"The Holy Mackerel",
+			"Unarmed Combat",
+		]
+	},
+	"tf_weapon_bat_giftwrap": {
+		"class": "Scout",
+		"slot": 3,
+		"display": "Wrap Assassin",
+		"all": [
+			"The Wrap Assassin"
+		]
+	},
+	"tf_weapon_bat_wood": {
+		"class": "Scout",
+		"slot": 3,
+		"display": "Sandman",
+		"all": [
+			"The Sandman"
+		]
+	},
+
+	# SOLDIER
+	"tf_weapon_rocketlauncher": {
+		"class": "Soldier",
+		"slot": 1,
+		"display": "Rocket Launcher",
+		"all": [
+			"Rocket Launcher",
+			"The Black Box",
+			"Rocket Jumper",
+			"The Liberty Launcher",
+			"The Original",
+			"The Beggar's Bazooka"
+		]
+	},
+	"tf_weapon_rocketlauncher_directhit": {
+		"class": "Soldier",
+		"slot": 1,
+		"display": "Direct Hit",
+		"all": [
+			"The Direct Hit"
+		]
+	},
+	"tf_weapon_rocketlauncher_airstrike": {
+		"class": "Soldier",
+		"slot": 1,
+		"display": "Air Strike",
+		"all": [
+			"The Air Strike"
+		]
+	},
+	"tf_weapon_shotgun_soldier": {
+		"class": "Soldier",
+		"slot": 2,
+		"display": "Shotguns",
+		"all": [
+			"Soldier's Shotgun",
+			"The Reserve Shooter",
+			"Panic Attack"
+		]
+	},
+	"tf_weapon_buff_item": {
+		"class": "Soldier",
+		"slot": 2,
+		"display": "Banners",
+		"all": [
+			"The Buff Banner",
+			"The Battalion's Backup",
+			"The Concheror"
+		]
+	},
+	"tf_weapon_raygun": {
+		"class": "Soldier",
+		"slot": 2,
+		"display": "Righteous Bison",
+		"all": [
+			"The Righteous Bison"
+		]
+	},
+	"tf_weapon_shovel": {
+		"class": "Soldier",
+		"slot": 3,
+		"display": "Melee",
+		"all": [
+			"Shovel",
+			"The Equalizer",
+			"The Pain Train",
+			"Frying Pan",
+			"The Half-Zatoichi",
+			"The Market Gardener",
+			"The Disciplinary Action",
+			"The Conscientious Objector",
+			"The Freedom Staff",
+			"The Bat Outta Hell",
+			"The Memory Maker",
+			"Gold Frying Pan",
+			"The Necro Smasher",
+			"The Crossing Guard",
+			"Prinny Machete"
+		]
+	},
+
+	# PYRO
+	"tf_weapon_flamethrower": {
+		"class": "Pyro",
+		"slot": 1,
+		"display": "Flamethrowers",
+		"all": [
+			"Flame Thrower",
+			"The Backburner",
+			"The Phlogistinator",
+			"The Rainblower",
+			"Nostromo Napalmer"
+		]
+	},
+	"tf_weapon_flamethrower_fireball": {
+		"class": "Pyro",
+		"slot": 1,
+		"display": "Dragon's Fury",
+		"all": [
+			"Dragon's Fury"
+		]
+	},
+	"tf_weapon_shotgun_pyro": {
+		"class": "Pyro",
+		"slot": 2,
+		"display": "Shotguns",
+		"all": [
+			"Pyro's Shotgun",
+			"The Reserve Shooter",
+			"Panic Attack",
+			"Thermal Thruster",
+			"Gas Passer"
+		]
+	},
+	"tf_weapon_flaregun": {
+		"class": "Pyro",
+		"slot": 2,
+		"display": "Flare Guns",
+		"all": [
+			"The Flare Gun",
+			"The Detonator",
+			"The Scorch Shot"
+		]
+	},
+	"tf_weapon_rocketpack": {
+		"class": "Pyro",
+		"slot": 2,
+		"display": "Thermal Thruster",
+		"all": [
+			"Thermal Thruster"
+		]
+	},
+	"tf_weapon_jar_gas": {
+		"class": "Pyro",
+		"slot": 2,
+		"display": "Gas Passer",
+		"all": [
+			"Gas Passer"
+		]
+	},
+	"tf_weapon_fireaxe": {
+		"class": "Pyro",
+		"slot": 3,
+		"display": "Most Melee",
+		"all": [		
+			"Fire Axe",
+			"The Axtinguisher",
+			"Homewrecker",
+			"The Powerjack",
+			"Frying Pan",
+			"The Back Scratcher",
+			"Sharpened Volcano Fragment",
+			"Saxxy",
+			"The Postal Pummeler",
+			"The Maul",
+			"The Conscientious Objector",
+			"The Third Degree",
+			"The Lollichop",
+			"The Freedom Staff",
+			"The Bat Outta Hell",
+			"The Memory Maker",
+			"The Ham Shank",
+			"Gold Frying Pan",
+			"The Necro Smasher",
+			"The Crossing Guard",
+			"Prinny Machete"
+		]
+	},
+	"tf_weapon_breakable_sign": {
+		"class": "Pyro",
+		"slot": 3,
+		"display": "Neon Annihilator",
+		"all": [
+			"Neon Annihilator"
+		]
+	},
+	"tf_weapon_slap": {
+		"class": "Pyro",
+		"slot": 3,
+		"display": "Hot Hand",
+		"all": [
+			"Hot Hand"
+		]
+	},
+
+	# DEMO
+
+	"tf_weapon_grenadelauncher": {
+		"class": "Demoman",
+		"slot": 1,
+		"display": "Grenade Launchers",
+		"all": [
+			"Grenade Launcher",
+			"The Loch-n-Load",
+			"The Iron Bomber"
+		]
+	},
+	"tf_weapon_cannon": {
+		"class": "Demoman",
+		"slot": 1,
+		"display": "Loose Cannon",
+		"all": [
+			"The Loose Cannon"
+		]
+	},
+	"tf_weapon_pipebomblauncher": {
+		"class": "Demoman",
+		"slot": 2,
+		"display": "Sticky Launchers",
+		"all": [
+			"Stickybomb Launcher",
+			"The Scottish Resistance",
+			"Sticky Jumper",
+			"The Quickiebomb Launcher"
+		]
+	},
+	"tf_weapon_bottle": {
+		"class": "Demoman",
+		"slot": 3,
+		"display": "Some Melee",
+		"all": [
+			"Bottle",
+			"The Scottish Handshake",
+			"Frying Pan",
+			"Saxxy",
+			"The Conscientious Objector",
+			"The Freedom Staff",
+			"The Bat Outta Hell",
+			"The Memory Maker",
+			"The Ham Shank",
+			"The Necro Smasher",
+			"The Crossing Guard",
+			"Prinny Machete"
+		]
+	},
+	"tf_weapon_sword": {
+		"class": "Demoman",
+		"slot": 3,
+		"display": "Swords",
+		"all": [
+			"The Eyelander",
+			"The Scotsman's Skullcutter",
+			"Horseless Headless Horsemann's Headtaker",
+			"The Claidheamh Mòr",
+			"The Persian Persuader",
+			"Nessie's Nine Iron"
+		]
+	},
+	"tf_weapon_katana": {
+		"class": "Demoman",
+		"slot": 3,
+		"display": "The Half-Zatoichi",
+		"all": [
+			"The Half-Zatoichi"
+		]
+	},
+
+	# HEAVY
+	"tf_weapon_minigun": {
+		"class": "Heavy",
+		"slot": 1,
+		"display": "Miniguns",
+		"all": [
+			"Minigun",
+			"Natascha",
+			"Iron Curtain",
+			"The Brass Beast",
+			"Tomislav",
+			"The Huo-Long Heater",
+			"Deflector (MvM only?)"
+		]
+	},
+	"tf_weapon_shotgun_hwg": {
+		"class": "Heavy",
+		"slot": 2,
+		"display": "Shotguns",
+		"all": [
+			"Heavy's Shotgun",
+			"The Family Business",
+			"Panic Attack"
+		]
+	},
+	"tf_weapon_lunchbox": {
+		"class": "Heavy",
+		"slot": 2,
+		"display": "Sandvich/Other food",
+		"all": [
+			"Sandvich",
+			"The Dalokohs Bar",
+			"The Buffalo Steak Sandvich",
+			"Fishcake",
+			"Robo-Sandvich",
+			"Second Banana"
+		]
+	},
+	"tf_weapon_fists": {
+		"class": "Heavy",
+		"slot": 3,
+		"display": "Melee",
+		"all": [
+			"Fists",
+			"The Killing Gloves of Boxing",
+			"Gloves of Running Urgently",
+			"Frying Pan",
+			"Warrior's Spirit",
+			"Fists of Steel",
+			"Saxxy",
+			"The Eviction Notice",
+			"The Conscientious Objector",
+			"Apoco-Fists",
+			"The Holiday Punch",
+			"The Freedom Staff",
+			"The Bat Outta Hell",
+			"The Memory Maker",
+			"The Ham Shank",
+			"Gold Frying Pan",
+			"The Bread Bite",
+			"The Necro Smasher",
+			"The Crossing Guard",
+			"Gloves of Running Urgently MvM",
+			"Prinny Machete"
+		]
+	},
+
+	# ENGINEER
+	"tf_weapon_shotgun_primary": {
+		"class": "Engineer",
+		"slot": 1,
+		"display": "Shotguns",
+		"all": [
+			"Engineer's Shotgun",
+			"The Widowmaker",
+			"Panic Attack"
+		]
+	},
+	"tf_weapon_sentry_revenge": {
+		"class": "Engineer",
+		"slot": 1,
+		"display": "The Frontier Justice",
+		"all": [
+			"The Frontier Justice"
+		]
+	},
+	"tf_weapon_drg_pomson": {
+		"class": "Engineer",
+		"slot": 1,
+		"display": "Pomson 6000",
+		"all": [
+			"The Pomson 6000"
+		]
+	},
+	"tf_weapon_shotgun_building_rescue": {
+		"class": "Engineer",
+		"slot": 1,
+		"display": "Rescue Ranger",
+		"all": [
+			"The Rescue Ranger"
+		]
+	},
+	"tf_weapon_pistol": {
+		"class": "Engineer",
+		"slot": 2,
+		"display": "Pistols",
+		"all": [
+			"Engineer's Pistol",
+			"Lugermorph",
+			"The C.A.P.P.E.R"
+		]
+	},
+	"tf_weapon_laser_pointer": {
+		"class": "Engineer",
+		"slot": 2,
+		"display": "Wranglers",
+		"all": [
+			"The Wrangler",
+			"The Giger Counter"
+		]
+	},
+	"tf_weapon_wrench": {
+		"class": "Engineer",
+		"slot": 3,
+		"display": "Most Melee",
+		"all": [
+			"Wrench",
+			"The Southern Hospitality",
+			"Golden Wrench",
+			"The Jag",
+			"Saxxy",
+			"The Eureka Effect",
+			"Gold Frying Pan",
+			"The Necro Smasher",
+			"Nutcracker",
+			"Prinny Machete"
+		]
+	},
+	"tf_weapon_robot_arm": {
+		"class": "Engineer",
+		"slot": 3,
+		"display": "Gunslinger",
+		"all": [
+			"The Gunslinger"
+		]
+	},
+
+	# MEDIC
+	"tf_weapon_syringegun_medic": {
+		"class": "Medic",
+		"slot": 1,
+		"display": "Syringe Guns",
+		"all": [
+			"Syringe Gun",
+			"The Blutsauger",
+			"The Overdose"
+		]
+	},
+	"tf_weapon_crossbow": {
+		"class": "Medic",
+		"slot": 1,
+		"display": "Crusader's Crossbow",
+		"all": [
+			"Crusader's Crossbow"
+		]
+	},
+	"tf_weapon_medigun": {
+		"class": "Medic",
+		"slot": 2,
+		"display": "Mediguns",
+		"all": [
+			"Medi Gun",
+			"The Kritzkrieg",
+			"The Quick-Fix",
+			"The Vaccinator"
+		]
+	},
+	"tf_weapon_bonesaw": {
+		"class": "Medic",
+		"slot": 3,
+		"display": "Melee",
+		"all": [
+			"Bonesaw",
+			"The Ubersaw",
+			"The Vita-Saw",
+			"Frying Pan",
+			"Amputator",
+			"The Solemn Vow",
+			"Saxxy",
+			"The Conscientious Objector",
+			"The Freedom Staff",
+			"The Bat Outta Hell",
+			"The Memory Maker",
+			"The Ham Shank",
+			"Gold Frying Pan",
+			"The Necro Smasher",
+			"The Crossing Guard",
+			"Prinny Machete"
+		]
+	},
+
+	# SNIPER
+	"tf_weapon_sniperrifle": {
+		"class": "Sniper",
+		"slot": 1,
+		"display": "Rifles",
+		"all": [
+			"Sniper Rifle",
+			"The Sydney Sleeper",
+			"The Bazaar Bargain",
+			"The Machina",
+			"The Hitman's Heatmaker",
+			"The AWPer Hand",
+			"Shooting Star"
+		]
+	},
+	"tf_weapon_sniperrifle": {
+		"class": "Sniper",
+		"slot": 1,
+		"display": "Bows",
+		"all": [
+			"The Huntsman",
+			"The Fortified Compound"
+		]
+	},
+	"tf_weapon_sniperrifle_decap": {
+		"class": "Sniper",
+		"slot": 1,
+		"display": "The Bazaar Bargain",
+		"all": [
+			"The Bazaar Bargain"
+		]
+	},
+	"tf_weapon_sniperrifle_classic": {
+		"class": "Sniper",
+		"slot": 1,
+		"display": "Classic",
+		"all": [
+			"The Classic"
+		]
+	},
+	"tf_weapon_smg": {
+		"class": "Sniper",
+		"slot": 2,
+		"display": "SMG",
+		"all": [
+			"SMG"
+		]
+	},
+	"tf_weapon_charged_smg": {
+		"class": "Sniper",
+		"slot": 2,
+		"display": "Cleaner's Carbine",
+		"all": [
+			"The Cleaner's Carbine"
+		]
+	},
+	"tf_weapon_jar": {
+		"class": "Sniper",
+		"slot": 2,
+		"display": "Jarate",
+		"all": [
+			"Jarate",
+			"The Self-Aware Beauty Mark"
+		]
+	},
+	"tf_weapon_club": {
+		"class": "Sniper",
+		"slot": 3,
+		"display": "Melee",
+		"all": [
+			"Kukri",
+			"The Tribalman's Shiv",
+			"The Bushwacka",
+			"Frying Pan",
+			"The Shahanshah",
+			"Saxxy",
+			"The Conscientious Objector",
+			"The Freedom Staff",
+			"The Bat Outta Hell",
+			"The Memory Maker",
+			"The Ham Shank",
+			"Gold Frying Pan",
+			"The Necro Smasher",
+			"The Crossing Guard",
+			"Prinny Machete"
+		]
+	},
+	"tf_weapon_revolver": {
+		"class": "Spy",
+		"slot": 1,
+		"display": "Revolvers",
+		"all": [
+			"Revolver",
+			"The Ambassador",
+			"Big Kill",
+			"L'Etranger",
+			"The Enforcer",
+			"The Diamondback"
+		]
+	},
+	"tf_weapon_builder": {
+		"class": "Spy",
+		"slot": 2,
+		"display": "Stock Sapper",
+		"all": [
+			"Sapper"
+		]
+	},
+	"tf_weapon_sapper": {
+		"class": "Spy",
+		"slot": 2,
+		"display": "Alternate Sappers",
+		"all": [
+			"The Red-Tape Recorder",
+			"The Ap-Sap",
+			"The Snack Attack"
+		]
+	},
+	"tf_weapon_knife": {
+		"class": "Spy",
+		"slot": 3,
+		"display": "Melee",
+		"all": [
+			"Knife",
+			"Your Eternal Reward",
+			"Conniver's Kunai",
+			"Saxxy",
+			"The Big Earner",
+			"The Wanga Prick",
+			"The Sharp Dresser",
+			"The Spy-cicle",
+			"The Black Rose",
+			"Gold Frying Pan",
+			"Prinny Machete"
+		]
+	}
+}
+reverse_associations = { "{}: {}".format(v["class"], v["display"]): { **v, "name": k } for (k,v) in weapon_associations.items() }
